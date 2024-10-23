c33917be8067f54e85ecc12f96ed20704be48a6f
ZWY <1939036524@qq.com>
2024-10-24 01:25:39 +0800

    update new model

--- myproject/api/views.py
+++ myproject/api/views.py
@@ -3,7 +3,7 @@ from rest_framework import viewsets, status
 from rest_framework.response import Response 
 from .models import AudioFile
 from .serializers import AudioFileSerializer
-from .isy5001_softvoting_implemention import extract_features,soft_voting,load_sklearn_model,load_model
+from .copy_of_isy5001_softvoting_implemention import extract_features,load_sklearn_model,predict
 import librosa
 import numpy as np
 import torch
@@ -28,7 +28,7 @@ class AudioFileViewSet(viewsets.ModelViewSet):
 
             # 获取上传的WAV文件�?�?
             audio_file = serializer.instance.audio_file
-            audio_file_path = audio_file.path
+            audio_path = audio_file.path
 
                
                
@@ -37,43 +37,33 @@ class AudioFileViewSet(viewsets.ModelViewSet):
 
 
             # 确保文件存在
-            if os.path.exists(audio_file_path):
+            if os.path.exists(audio_path):
                 # 使用 librosa 加载音频文件
-                audio_data, sr = librosa.load(audio_file_path, sr=None)
+                audio_data, sr = librosa.load(audio_path, sr=None)
                 print("音频数据加载成功！")
             else:
                 print("文件不存在，请检查路径。")
 
 
+            audio_data, sr = librosa.load(audio_path, sr=None)
+
             features = extract_features(audio_data, sr)
 
-            rf_model_path = r'D:\project\project5001-backend-master\myproject\detection\rf_classifier.joblib'
-            knn_model_path = r'D:\project\project5001-backend-master\myproject\detection\knn_classifier.joblib'
-            dnn_model_path = r'D:\project\project5001-backend-master\myproject\detection\dnn_model (1).pth'
-            svm_model_path = r'D:\project\project5001-backend-master\myproject\detection\best_svm_model (2).joblib'
-            device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+            voting_classifier= load_sklearn_model(r'D:\project\project5001-backend-master\myproject\detection\voting_classifier.joblib')
+            voting_probabilities = predict(voting_classifier, features)
 
-            rf_model = load_sklearn_model(rf_model_path)
-            knn_model = load_sklearn_model(knn_model_path)
-            svm_model = load_sklearn_model(svm_model_path)
-            dnn_model = load_model(dnn_model_path)
-            dnn_model.to(device)
+            # Determine the predicted label based on the voting classifier's probabilities
+            predicted_label_voting = np.argmax(voting_probabilities)
 
-            models = [rf_model, knn_model, svm_model, dnn_model]
-            avg_probabilities, predicted_label = soft_voting(models, features)
-            print(f"Predicted Label: {predicted_label}")
-            print(f"Average Probabilities: {avg_probabilities}")
-            labels = ['deception', 'true']
-            deception_probability = avg_probabilities[0]
-            print(f"Deception Probability: {deception_probability}")
-
-          
-          
-          
+            # Print the results
+            print(f"Voting Classifier - Predicted Label: {predicted_label_voting}, Probabilities: {voting_probabilities}")
+            labels = [ 'Deception','Truth']
+            # Print the predicted label and lying probability for the Voting classifier
+            print(f"\nVoting Classifier Prediction: {labels[predicted_label_voting]}, Lying Probability: {voting_probabilities[0]}")         
             return Response({
-                'Predicted Label': predicted_label,
-                'Average Probabilities': avg_probabilities,
-                'Deception Probability': deception_probability}, status=status.HTTP_201_CREATED)
+                'Predicted Label': predicted_label_voting,
+                'Voting Classifier Prediction': labels[predicted_label_voting],
+                'Deception Probability': voting_probabilities[0]}, status=status.HTTP_201_CREATED)
         else:
             print(serializer.errors)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
